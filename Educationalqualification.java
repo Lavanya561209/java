11e0c4ff4d9aed806ada1a96b1b631a9698f6e6a
Lavanya561209 <lavanyaxworkzoutlook@gmail.com>
2025-08-08 16:22:08 +0530

    java program

---
+++ Educationalqualification.java
@@ -0,0 +1,9 @@
+class Educationalqualification{
+ public static void main(String args[]){
+  String x = "BCA";
+  String y = "Maharani Cluster Universtiy";
+  String z = "Address";
+  int age =  20;
+ }
+}
+  
