ba53d82721f82c84cbb3bfb8efdb563df99b4595
Lavanya561209 <lavanyaxworkzoutlook@gmail.com>
2025-08-19 11:09:22 +0530

    Add files via upload

---
+++ Vivoy39.java
@@ -0,0 +1,9 @@
+class Vivoy39{
+ public static void main(String []args){
+  byte size = 125;
+  if(size>=120){
+	  System.out.println("size of phone is:"+size);
+  }
+     System.out.println("size of an vivo Y39 phone is:"+size+"");
+  }
+ }
