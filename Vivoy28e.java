766231e1a5298434cc153a3da41361ec67742606
Lavanya561209 <lavanyaxworkzoutlook@gmail.com>
2025-08-19 11:07:54 +0530

    Add files via upload

---
+++ Vivoy28e.java
@@ -0,0 +1,9 @@
+class Vivoy28e{
+ public static void main(String []args){
+  byte size = 64;
+  if(size<=56){
+	  System.out.println("size of mobile is:"+size);
+  }
+      System.out.println("size of an vivo Y28e phone is:"+size+"");
+  }
+ }
