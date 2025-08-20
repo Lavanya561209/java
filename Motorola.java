9f9477f7f253dbf3974e2eb521bfbb22e4a3b948
Lavanya561209 <lavanyaxworkzoutlook@gmail.com>
2025-08-20 09:52:33 +0530

    Add files via upload

---
+++ Motorola.java
@@ -0,0 +1,40 @@
+class Motorola{
+ public static void main(String []args){
+ int price=15999;
+ System.out.println(price);
+ System.out.println("The cost of this product is:"+price);
+ System.out.println("The cost of this product is");
+ long phoneNumber=2345567812l;
+ System.out.println(phoneNumber);
+ System.out.println("The reference number is:"+phoneNumber);
+ System.out.println("The reference number is");
+ boolean isAvailable=true;
+ System.out.println(isAvailable);
+ System.out.println("The product available is:"+isAvailable);
+ System.out.println("The product available is");
+ char grade='A';
+ System.out.println(grade);
+ System.out.println("The grade of this product is:"+grade);
+ System.out.println("The grade of this product is");
+ float gst=127.892f;
+ System.out.println(gst);
+ System.out.println("The cost of gst is:"+gst);
+ System.out.println("The cost of gst is");
+ double reviews=124;
+ System.out.println(reviews);
+ System.out.println("The reviews of this product is:"+reviews);
+ System.out.println("The reviews of this product is");
+ byte ram=32;
+ System.out.println("The size of ram is:"+ram);
+ System.out.println(ram);
+ System.out.println("The size of ram is");
+ short weight=181;
+ System.out.println(weight);
+ System.out.println("The weight of this product is:"+weight);
+ System.out.println("The weight of this product is");
+ String [] purchase={"Limited stocks is available","Buy fast"};
+ System.out.println(purchase[1]);
+ System.out.println("The purchase of this product is:"+purchase[0]);
+ System.out.println("The purachse of this product is");
+ }
+}
