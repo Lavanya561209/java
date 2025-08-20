cf2a4aced768696db4f2e422f267ec57f8269025
Lavanya561209 <lavanyaxworkzoutlook@gmail.com>
2025-08-20 22:01:30 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,16 @@
+class Car{
+	public static void main(String []args){
+		String [] safety={"belt","hands","mobile","wrong route","do not distrub"};
+		safety [0]="Seat belt";
+		safety [1]="Don't keep hands or head out of the window";
+		safety [2]="Don't use mobile while driving the car";
+		safety [3]="Go on the right way";
+		safety [4]="Be careful while driving the car";
+		System.out.println("The safety measures are:"+safety[0]);
+		System.out.println("The safety measures are:"+safety[1]);
+		System.out.println("The safety measures are:"+safety[2]);
+		System.out.println("The safety measures are:"+safety[3]);
+		System.out.println("The safety measures are:"+safety[4]);
+	}
+		
+}
