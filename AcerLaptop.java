0164a97d68fe3ddc9cab1c26da5dc55e591ebafb
Lavanya561209 <lavanyaxworkzoutlook@gmail.com>
2025-08-20 13:18:36 +0530

    Add files via upload

---
+++ AcerLaptop.java
@@ -0,0 +1,49 @@
+class AcerLaptop{
+ public static void main(String [] args){
+  int price=22000;
+  price=24000;
+  System.out.println(price);
+  System.out.println("The cost of price is:"+price);
+  System.out.println("The cost of price is");
+  long contactNumber=974563212l;
+  contactNumber=8945627720l;
+  System.out.println(contactNumber);
+  System.out.println("The reference number is:"+contactNumber);
+  System.out.println("The reference number is");
+  boolean isAvailable=true;
+  isAvailable=false;
+  System.out.println("The product available is:"+isAvailable);
+  System.out.println(isAvailable);
+  System.out.println("The product available is");
+  char grade='O';
+  grade='A';
+  System.out.println(grade);
+  System.out.println("The grade of product is:"+grade);
+  System.out.println("The grade of product is");
+  float gst=400.456f;
+  gst=200.456f;
+  System.out.println(gst);
+  System.out.println("The cost of gst is:"+gst);
+  System.out.println("The cost of gst is");
+  double reviews=27d;
+  reviews=80d;
+  System.out.println("The reviews  is:"+reviews);
+  System.out.println("The reviews is");
+  System.out.println(reviews);
+  byte ram=12;
+  ram=8;
+  System.out.println(ram);
+  System.out.println("The size of ram is:"+ram);
+  System.out.println("The size of ram is");
+  short weight=700;
+  weight=1500;
+  System.out.println(weight);
+  System.out.println("The weight of product is:"+weight);
+  System.out.println("The weight of product is");
+  String[] display={"Anti Glare","Easy Touch","High resistance"};
+  display [0]="LCD";
+  System.out.println(display[0]);
+  System.out.println("The display highlights of product is:"+display[0]);
+  System.out.println("The display highlights of product is"); 
+  }
+ }
