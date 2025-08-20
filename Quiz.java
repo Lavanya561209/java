2021bfa3ee38195b89664e19ae203f60c275c22d
Lavanya561209 <lavanyaxworkzoutlook@gmail.com>
2025-08-20 22:00:55 +0530

    Add files via upload

---
+++ Quiz.java
@@ -0,0 +1,16 @@
+class Quiz{
+ public static void main(String args[]){
+  String conditions []={"Chair","Pen","Pencil","Student","Play"};
+   conditions [0]="Dress Code";
+   conditions [1]="On-time";
+   conditions [2]="Group Discussion";
+   conditions [3]="Limited students";
+   conditions [4]="No use for rough work";
+   System.out.println("The regulations provided in the quiz is:-"+conditions[0]);
+   System.out.println("The conditions provided in the quiz is:-"+conditions[1]);
+   System.out.println("The conditions provided in the quiz is:-"+conditions[2]);
+   System.out.println("The conditions provided in the quiz is:-"+conditions[3]);
+   System.out.println("The conditions provided in the quiz is:-"+conditions[4]);
+   }
+ }
+   
