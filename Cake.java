7168406013c9c52fda7df6e74400168fa09dbe86
Lavanya561209 <lavanyaxworkzoutlook@gmail.com>
2025-08-20 19:27:52 +0530

    Add files via upload

---
+++ Cake.java
@@ -0,0 +1,16 @@
+class Cake{
+ public static void main(String preery[]){
+  String strawberryCupCake[]={"Milk","Strawberries","Butter","Sugar","Eggs","Sour Cream","Baking Powder","Flour","Baking Soda","Salt","Powdered Sugar"};
+    System.out.println("Distinctive  ingredients to make cake is:"+strawberryCupCake [0]);
+    System.out.println("Distinctive  ingredients to make cake is:"+strawberryCupCake [1]);
+    System.out.println("Distinctive  ingredients to make cake is:"+strawberryCupCake [2]);
+    System.out.println("Distinctive  ingredients to make cake is:"+strawberryCupCake [3]);
+    System.out.println("Distinctive  ingredients to make cake is:"+strawberryCupCake [4]);
+    System.out.println("Distinctive  ingredients to make cake is:"+strawberryCupCake [5]);
+    System.out.println("Distinctive  ingredients to make cake is:"+strawberryCupCake [6]);
+    System.out.println("Distinctive  ingredients to make cake is:"+strawberryCupCake [7]);
+    System.out.println("Distinctive  ingredients to make cake is:"+strawberryCupCake [8]);
+    System.out.println("Distinctive  ingredients to make cake is:"+strawberryCupCake[9]);
+    System.out.println("Distinctive  ingredients to make cake is:"+strawberryCupCake [10]);
+	}
+}
