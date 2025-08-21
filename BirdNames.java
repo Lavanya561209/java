eb3710cc2de3d99223912d42bc83bcd8946c5d60
Lavanya561209 <lavanyaxworkzoutlook@gmail.com>
2025-08-21 21:48:25 +0530

    Add files via upload

---
+++ BirdNames.java
@@ -0,0 +1,10 @@
+class BirdNames{
+	public static void main(String []args){
+		String [] called={"Peacock","Sparrow","Parrot","Pegion","Penguin","Humming Bird"};
+		int i=0;
+		do{
+			System.out.println(called[i]);
+			i++;
+		}while(i<called.length);
+	}
+}
