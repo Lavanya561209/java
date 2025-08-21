ff521f944988251d6b2805e97eae3d866b528c42
Lavanya561209 <lavanyaxworkzoutlook@gmail.com>
2025-08-21 21:49:58 +0530

    Add files via upload

---
+++ ShopName.java
@@ -0,0 +1,10 @@
+class ShopName{
+	public static void main(String args[]){
+		String names[]={"Lakshmi Store","Ganesh Medicals","Dharani Store","Amruth Medicals","Swasthi Store"};
+		int i=0;
+		do{
+			System.out.println(names[i]);
+			i++;
+		}while(i<names.length);
+	}
+}
