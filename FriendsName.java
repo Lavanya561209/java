cc992a919e5feaf38d1b53df7cee865afa67ce87
Lavanya561209 <lavanyaxworkzoutlook@gmail.com>
2025-08-21 21:48:52 +0530

    Add files via upload

---
+++ FriendsName.java
@@ -0,0 +1,10 @@
+class FriendsName{
+	public static void main(String []args){
+		String []names={"Khizra","Saibha","Sumithra","Meghana","Chandana"};
+		int i=0;
+		do{
+			System.out.println(names[i]);
+			i++;
+		}while(i<names.length);
+	}
+}
