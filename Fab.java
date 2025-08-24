077c893c56eb5128947613c27c7b0487bad828d5
Lavanya561209 <lavanyaxworkzoutlook@gmail.com>
2025-08-24 22:55:39 +0530

    Add files via upload

---
+++ Fab.java
@@ -0,0 +1,24 @@
+class Fab{
+	 static  String names="India";
+	 static  char capacity='L';
+     static  byte netQuanitity=10;
+	static  long phoneNumbers=8847374592l;
+	static  int price=89;
+	static float productLength=0.5f;
+	static short liters=1000;
+	static boolean isAvailable=true;
+	static double followers=6d;
+	static String[] information={"Not availabe","Godrej","Liquid Detergent","Front Load"};
+	public static void main (String[] args){
+		System.out.println("The name of the country belongs too:"+names);
+		System.out.println("The capacity of the product is:"+capacity);
+		System.out.println("The quantity of the product  is:"+netQuanitity);
+		System.out.println("The different contact number is:"+phoneNumbers);
+		System.out.println("The cost of product price  is:"+price);
+		System.out.println("The different measures of the product is:"+productLength);
+		System.out.println("The capacity of the product is:"+liters);
+		System.out.println("The product of stock ratings are:"+isAvailable);
+		System.out.println("How the product craze is:"+followers);
+		System.out.println("About product description is:"+information[3]);
+	}
+}
