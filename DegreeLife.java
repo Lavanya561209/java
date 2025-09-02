3cf81a9227e4d484d45339e23bb70239ba81c198
Lavanya561209 <lavanyaxworkzoutlook@gmail.com>
2025-09-02 22:28:12 +0530

    Add files via upload

---
+++ DegreeLife.java
@@ -0,0 +1,41 @@
+class DegreeLife{
+  public static void firstYear(){
+	  System.out.println("I have joined the college");
+  }
+  public static void firstSem(){
+	  System.out.println("I learned some subjects");
+  }  
+  
+  public static void secondSem(){
+	  System.out.println("I have faced exams");
+  }
+  public static void secondYear(){
+	  System.out.println("I have learned new subjects");
+  }
+  public static void thirdSem(){
+	  System.out.println("I have learnt still more subjects");
+  }
+  public static void fourthSem(){
+	  System.out.println("I have learnt some different subjects");
+  }
+  public static void thirdYear(){
+	  System.out.println("I have faced new challenges");
+  }
+  public static void fifthSem(){
+	  System.out.println("I have joined unnati foundation");
+  }
+  public static void sixthSem(){
+	  System.out.println("I have made an new project");
+  }
+  public static void main(String[] args){
+	  firstYear();
+	  firstSem();
+	  secondSem();
+	  secondYear();
+	  thirdSem();
+	  fourthSem();
+	  thirdYear();
+	  fifthSem();
+	  sixthSem();
+  }
+}
