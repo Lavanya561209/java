b430028bd6df7391222913f13fe88f9303c7d2ed
Lavanya561209 <lavanyaxworkzoutlook@gmail.com>
2025-09-11 11:55:11 +0530

    Add files via upload

---
+++ Electrical.java
@@ -0,0 +1,18 @@
+class Electrical{
+	public static void main(String[] args){
+		Plugin.reviews();
+		Plugin.height();
+		Plugin.cost();
+		Plugin.phoneNumber();
+		Plugin.availableProduct();
+		Plugin.quality();
+		Plugin.gst();
+		Plugin.subscribers();
+		Plugin.names();
+		System.out.println("====================");
+		Plugin.firsttime();
+	    System.out.println("====================");
+		Plugin.fresher();
+		Plugin.experience();
+	}
+}
---
+++ Information.java
@@ -0,0 +1,11 @@
+class Information{
+	public static void main(String[] args){
+		System.out.println(AboutBank.price);
+		System.out.println(AboutBank.name);
+		System.out.println(AboutBank.isAvailable);
+		System.out.println(AboutBank.personAge);
+		System.out.println(AboutBank.phoneNumber);
+		System.out.println("===================");
+	     AboutBank.customerName();
+	}
+}
